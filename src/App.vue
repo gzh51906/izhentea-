c69185d6f3bd21aad951841bd1024ad455e864f3
liwende <liwende123@456.com>
2019-09-05 17:32:53 +0800

    后台管理系统初始化，可以进行路由跳转切换---liwende

--- src/App.vue
+++ src/App.vue
@@ -10,24 +10,30 @@ import Vue from "vue";
 //引入ui组件vant
 import Vant from "vant";
 import "vant/lib/index.css";
-import "./rem.js"
+import "./rem.js";
 
+//引入vant组件
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+Vue.use(Vant);
+
+Vue.use(Antd);
 
 export default {
-  name: 'app',
-  components: {
-   
-  }
-}
+  name: "app",
+  components: {}
+};
 </script>
 
 <style>
+html,
+body {
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  background: ;
 }
 </style>
